ac5865fa9699719e28c55725c0732b2a8a687245
Jeremy Nearing <jeremy07836@gmail.com>
2018-10-24 12:04:56 -0400

    Added Ravenous Website
    Added the build of the website Ravenous. Added instructions to run the website in the dev README file.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dev
+
+Ravenous Website
+  To run the website ravenous I used the chrome plugin: 'Allow-Control-Allow-Origin'  
+  To run in local enviroment use:
+    1. npm install -g serve
+    2. serve -s build
---
+++ Ravenous/build/precache-manifest.0ddedc8b4cbf0fb7877cdaa52c13be61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23de04675a5b386fb288",
+    "url": "/static/js/main.23de0467.chunk.js"
+  },
+  {
+    "revision": "e1ada48e35de0e3b8be4",
+    "url": "/static/js/1.e1ada48e.chunk.js"
+  },
+  {
+    "revision": "23de04675a5b386fb288",
+    "url": "/static/css/main.85293a90.chunk.css"
+  },
+  {
+    "revision": "35f9f6dfb25b7f5befdf6b3a19654e17",
+    "url": "/index.html"
+  }
+];
